27fce752290d6adae11e92bbb403423cfece0e6d
Kacper Gil <kacpergil1610@gmail.com>
2022-12-05 20:39:35 +0100

    dodanie dowhile

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,40 +6,47 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Witaj w naszym banku!");
         System.out.println("Podaj swoje imie:");
-        Scanner scanner=new Scanner(System.in);
-        String name=scanner.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
         System.out.println("Podaj swoje nazwisko:");
-        String surname=scanner.nextLine();
+        String surname = scanner.nextLine();
         System.out.println("Podaj swój pesel:");
-        int pesel=scanner.nextInt();
-        String pesel2=String.valueOf(pesel);
-        if (pesel2.length()>10||pesel2.length()<9){
+        int pesel = scanner.nextInt();
+        String pesel2 = String.valueOf(pesel);
+        if (pesel2.length() > 10 || pesel2.length() < 9) {
             System.out.println("Podałeś źle pesel");
             return;
         }
 
-        User user1=new User(name, surname, pesel);
+        User user1 = new User(name, surname, pesel);
+
+
+        int dzialanie;
+        do {
         System.out.println("Wybierz działanie\n 1.Informacje o koncie\n 2.Wpłata pieniędzy\n 3.Wypłata pieniędzy \n 4.Wyjście");
-       int dzialanie=scanner.nextInt();
-         switch (dzialanie){
-            case 1:
-                System.out.println("Informacje o koncie!");
-                user1.userDetails();
-                break;
-             case 2:
-                 System.out.println("Ile chcesz wpłacić pieniędzy?: ");
-                 double moneytodeposit=scanner.nextDouble();
-                 user1.deposit(moneytodeposit);
-                 System.out.println("Twoje saldo teraz: "+user1.getSaldo());
-                break;
-             case 3:
-                 System.out.println("Ile chcesz wypłacić pieniędzy?: ");
-                 double moneydraw=scanner.nextDouble();
-                 user1.withdraw(moneydraw);
-                 System.out.println("Twoje saldo teraz: "+ user1.getSaldo());
-                 break;
-             case 4:
-                 break;
-         }
+        dzialanie = scanner.nextInt();
+
+            switch (dzialanie) {
+                case 1:
+                    System.out.println("Informacje o koncie!");
+                    user1.userDetails();
+                    break;
+                case 2:
+                    System.out.println("Ile chcesz wpłacić pieniędzy?: ");
+                    double moneytodeposit = scanner.nextDouble();
+                    user1.deposit(moneytodeposit);
+                    System.out.println("Twoje saldo teraz: " + user1.getSaldo());
+                    break;
+                case 3:
+                    System.out.println("Ile chcesz wypłacić pieniędzy?: ");
+                    double moneydraw = scanner.nextDouble();
+                    user1.withdraw(moneydraw);
+                    System.out.println("Twoje saldo teraz: " + user1.getSaldo());
+                    break;
+                case 4:
+                    System.out.println("Do zobaczenia!");
+                    break;
+            }
+        } while(dzialanie!=4);
     }
 }
