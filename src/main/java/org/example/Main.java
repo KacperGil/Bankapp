09ffc38fd72c170a4bef3ecb8d007c60dd226aff
Kacper Gil <kacpergil1610@gmail.com>
2022-12-05 18:55:20 +0100

    nickname generator, poprawa konstruktora

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,29 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Witaj w naszym banku!");
+        System.out.println("Podaj swoje imie:");
+        Scanner scanner=new Scanner(System.in);
+        String name=scanner.nextLine();
+        System.out.println("Podaj swoje nazwisko:");
+        String surname=scanner.nextLine();
+        System.out.println("Podaj swój pesel:");
+        int pesel=scanner.nextInt();
+        String pesel2=String.valueOf(pesel);
+        if (pesel2.length()>10){
+            System.out.println("Podałeś źle pesel");
+        }
 
+        User user1=new User(name, surname, pesel);
+        System.out.println("Wybierz działanie\n 1.Informacje o koncie\n 2.Wpłata pieniędzy\n 3.Wypłata pieniędzy \n 4.Wyjście");
+//        int dzialanie=scanner.nextInt();
+//         switch (dzialanie){
+//            case 1:
+//                System.out.println(name+" "+surname);
+//                System.out.println(pesel);
+//        }
     }
 }
--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,4 +1,23 @@
 package org.example;
 
 public class User {
+    private String name;
+    private String surname;
+    private String nickname;
+    private int pesel;
+    private int accountNumber;
+    private double saldo;
+
+    public User(String name, String surname, int pesel) {
+        this.name = name;
+        this.surname = surname;
+        this.pesel = pesel;
+        this.nickname=nicknameGenerator(name,surname);
+        this.saldo=100;
+        this.accountNumber=1;
+    }
+    public String nicknameGenerator(String name, String surname){
+        return (name.substring(0,3)+surname.substring(0,3));
+    }
+
 }
